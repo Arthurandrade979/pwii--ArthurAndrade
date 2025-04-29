3b30f9bc6394872db2a153e7aa399d8db5d5d142
Arthurandrade979 <arthur.renatoandrade@gmail.com>
2025-04-29 16:30:42 -0300

    Aula de lacos php

---
+++ aula 4/lacosderepeticao.php
@@ -0,0 +1,29 @@
+<?php
+ $i = 1;
+
+ while ($i < 6){
+
+     echo $contador, "<br>";
+     $contador++;
+ }
+
+
+ //do while
+
+ $contador2 = 1;
+
+    do {
+        echo $contadorii;
+        $contadorii++;
+    } while ($contadorii < 6);
+
+      //for
+
+        echo "<br> For <br>";
+
+        for ($x - 0; $x <=10; $x++) {
+            echo "O numero é: $x <br>"; 
+        }
+
+?>
+
