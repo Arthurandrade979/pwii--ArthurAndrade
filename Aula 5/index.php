2bef2f32a57d0e585f6d72e1c24a8b7d64f11821
ArthurAndrade <arthur.renatoandrade@gmail.com>
2025-05-06 16:30:10 -0300

    Add files via upload

---
+++ Aula 5/index.php
@@ -0,0 +1,11 @@
+<html>
+   <body>
+
+    <form action="mensagem.php" method="POST">
+        digite sua mensagem:
+        <input type="text" name="msg"><br>
+        <input type="submit">
+    </form>
+
+   </body>
+</html>
